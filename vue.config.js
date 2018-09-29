2b4427e271a39d7bda73ccad937c24207f782931
Sendya <18x@loacg.com>
2018-09-29 16:29:13 +0800

    updated: mock server by:ihx.me

--- vue.config.js
+++ vue.config.js
@@ -31,9 +31,11 @@ module.exports = {
         modifyVars: {
           /* less 变量覆盖，用于自定义 ant design 主题 */
 
+          /*
           'primary-color': '#F5222D',
           'link-color': '#F5222D',
           'border-radius-base': '4px',
+          */
         },
         javascriptEnabled: true,
       }
@@ -42,12 +44,12 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'https://www.easy-mock.com/mock/5b7bce071f130e5b7fe8cd7d/antd-pro',
-        ws: true,
+        target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
+        ws: false,
         changeOrigin: true
       },
       '/gateway': {
-        target: 'https://www.easy-mock.com/mock/5b7bce071f130e5b7fe8cd7d/antd-pro',
+        target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
         changeOrigin: true,
         pathRewrite: {
           '^/gateway': '/api'
