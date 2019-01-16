97b55a4bf3be09106c2c7f2b456facde18ac089a
Sendya <18x@loacg.com>
2019-01-16 22:38:48 +0800

    refactor: login

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ const api = {
     Logout: '/auth/logout',
     ForgePassword: '/auth/forge-password',
     Register: '/auth/register',
+    twoStepCode: '/auth/2step-code',
     SendSms: '/account/sms',
     SendSmsErr: '/account/sms_err',
     // get my info
--- src/api/login.js
+++ src/api/login.js
@@ -47,3 +47,15 @@ export function logout() {
     }
   })
 }
+
+/**
+ * get user 2step code open?
+ * @param parameter {*}
+ */
+export function get2step(parameter) {
+  return axios({
+    url: api.twoStepCode,
+    method: 'post',
+    data: parameter
+  })
+}
