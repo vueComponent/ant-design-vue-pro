7ddaf39d08c79c218cc563045cf501a215a1f217
Sendya <18x@loacg.com>
2019-02-19 11:07:28 +0800

    feat: Load on demand (loadfile)
    Signed-off-by: Sendya <18x@loacg.com>

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,12 @@ module.exports = {
   presets: [
     '@vue/app'
   ]
+  // ,
+  // plugins: [
+  //   [ 'import', {
+  //     'libraryName': 'ant-design-vue',
+  //     'libraryDirectory': 'es',
+  //     'style': true // `style: true` 会加载 less 文件
+  //   } ]
+  // ]
 }
