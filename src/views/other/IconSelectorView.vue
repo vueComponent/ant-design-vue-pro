b661cde01097871b103a89fa1046f77b1f7ec0c4
Sendya <18x@loacg.com>
2019-02-24 16:38:27 +0800

    feat: add icon-selector component

---
+++ src/views/other/IconSelectorView.vue
@@ -0,0 +1,27 @@
+<template>
+  <a-card :body-style="{padding: '24px 32px'}" :bordered="false">
+    <icon-selector @change="handleIconChange"/>
+  </a-card>
+</template>
+
+<script>
+import IconSelector from '@/components/IconSelector'
+
+export default {
+  name: 'IconSelectorView',
+  components: {
+    IconSelector
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    handleIconChange (icon) {
+      console.log('change Icon', icon)
+      this.$message.info(<span>选中图标 <code>{icon}</code></span>)
+    }
+  }
+}
+</script>
