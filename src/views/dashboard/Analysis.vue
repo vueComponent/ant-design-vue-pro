3cbed2bdda51bf818a65f93f98fbae0f0ae828a9
Sendya <18x@loacg.com>
2019-03-27 01:06:03 +0800

    chore: fix eslint

--- src/views/dashboard/Analysis.vue
+++ src/views/dashboard/Analysis.vue
@@ -185,8 +185,8 @@
             <v-chart :force-fit="true" :height="350" :data="pieData" :scale="pieScale">
               <v-tooltip :showTitle="false" dataKey="item*percent" />
               <v-axis />
-              <v-legend dataKey="item"  position="right" :offsetX="-140"/>
-              <v-pie position="percent" color="item" :vStyle="pieStyle"  />
+              <v-legend dataKey="item" position="right" :offsetX="-140"/>
+              <v-pie position="percent" color="item" :vStyle="pieStyle" />
               <v-coord type="theta" :radius="0.75" :innerRadius="0.6" />
             </v-chart>
           </div>
