f92d2cb66b53496e9f42ae5561db5ca25857beb7
Sendya <18x@loacg.com>
2019-04-02 15:17:59 +0800

    fix: set sidebar mixin

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -57,13 +57,16 @@ const AppDeviceEnquire = {
       switch (deviceType) {
         case DEVICE_TYPE.DESKTOP:
           $store.commit('TOGGLE_DEVICE', 'desktop')
+          $store.dispatch('setSidebar', true)
           break
         case DEVICE_TYPE.TABLET:
           $store.commit('TOGGLE_DEVICE', 'tablet')
+          $store.dispatch('setSidebar', false)
           break
         case DEVICE_TYPE.MOBILE:
         default:
           $store.commit('TOGGLE_DEVICE', 'mobile')
+          $store.dispatch('setSidebar', true)
           break
       }
     })
