38c420f9bd00c9fc04fb72de391c5675ae7ea9ab
Sendya <18x@loacg.com>
2019-04-09 01:38:36 +0800

    feat: add base mock

---
+++ src/mock/index.js
@@ -0,0 +1,8 @@
+import Mock from 'mockjs'
+
+import './services/auth'
+import './services/user'
+
+Mock.setup({
+  timeout: 800 // setter delay time
+})
---
+++ src/mock/services/auth.js
@@ -0,0 +1,38 @@
+import Mock from 'mockjs'
+import { builder, getBody } from '../util'
+
+const username = ['admin', 'user', 'super']
+const password = ['21232f297a57a5a743894a0e4a801fc3', '8914de686ab28dc22f30d3d8e107ff6c'] // admin, ant.design
+
+const login = (options) => {
+  const body = getBody(options)
+  console.log('mock: body', body)
+  if (!username.includes(body.username) || !password.includes(body.password)) {
+    return builder({}, '账户或密码错误', 401)
+  }
+
+  return builder({
+    'id': Mock.mock('@guid'),
+    'name': Mock.mock('@name'),
+    'username': 'admin',
+    'password': '',
+    'avatar': 'https://gw.alipayobjects.com/zos/rmsportal/jZUIxmJycoymBprLOUbT.png',
+    'status': 1,
+    'telephone': '',
+    'lastLoginIp': '27.154.74.117',
+    'lastLoginTime': 1534837621348,
+    'creatorId': 'admin',
+    'createTime': 1497160610259,
+    'deleted': 0,
+    'roleId': 'admin',
+    'lang': 'zh-CN',
+    'token': '4291d7da9005377ec9aec4a71ea837f'
+  }, '', 200, { 'Custom-Header': Mock.mock('@guid') })
+}
+
+const logout = () => {
+  return builder({}, '[测试接口] 注销成功')
+}
+
+Mock.mock(/\/auth\/login/, 'post', login)
+Mock.mock(/\/auth\/logout/, 'post', logout)
