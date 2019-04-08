4259cf22ce2119f013129fee880afbf45cd85884
阿凌 <510415008@qq.com>
2019-04-08 16:58:12 +0800

    fix: Inheritance a-table  is invalid
    fix: Inheritance a-table  is invalid

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -11,7 +11,7 @@ export default {
 
       localLoading: false,
       localDataSource: [],
-      localPagination: Object.assign({}, T.props.pagination)
+      localPagination: Object.assign({}, this.pagination)
     }
   },
   props: Object.assign({}, T.props, {
