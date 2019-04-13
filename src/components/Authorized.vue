bb6746229d9ff99a6313e4263fe781c67eb6ccc6
tangjinzhou <415800467@qq.com>
2019-04-13 21:51:49 +0800

    0.0.7

---
+++ src/components/Authorized.vue
@@ -0,0 +1,16 @@
+<script>
+import { check } from "../utils/auth";
+export default {
+  functional: true,
+  props: {
+    authority: {
+      type: Array,
+      required: true
+    }
+  },
+  render(h, context) {
+    const { props, scopedSlots } = context;
+    return check(props.authority) ? scopedSlots.default() : null;
+  }
+};
+</script>
---
+++ src/directives/auth.js
@@ -0,0 +1,13 @@
+import { check } from "../utils/auth";
+
+function install(Vue, options = {}) {
+  Vue.directive(options.name || "auth", {
+    inserted(el, binding) {
+      if (!check(binding.value)) {
+        el.parentNode && el.parentNode.removeChild(el);
+      }
+    }
+  });
+}
+
+export default { install };
