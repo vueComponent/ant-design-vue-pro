562ef5cd5b52a27d10a3fba77429d8f1169f7ad2
Sendya <18x@loacg.com>
2019-04-14 08:07:37 +0800

    fix: table Maximum call stack size

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -145,7 +145,7 @@ export default {
               this.localPagination.pageSize
           })
           // 为防止删除数据后导致页面当前页面数据长度为 0 ,自动翻页到上一页
-          if (r.data.length === 0 && this.localPagination.current !== 1) {
+          if (r.data.length === 0 && this.localPagination.current > 1) {
             this.localPagination.current--
             this.loadData()
             return
