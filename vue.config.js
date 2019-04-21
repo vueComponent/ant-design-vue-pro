2f9559ef7d673e324d1e30576a977fb718a53c6d
tangjinzhou <415800467@qq.com>
2019-04-21 16:31:56 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,8 @@
-const path = require("path");
 module.exports = {
   css: {
     loaderOptions: {
       less: {
         javascriptEnabled: true
-      }
-    }
-  },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        "@": path.join(__dirname, "src")
       }
     }
   },
