c59e32bad1d06e562e68da9ecd650aa6f4b27421
Sendya <18x@loacg.com>
2019-09-10 21:31:16 +0800

    style: eslint rule fix

--- src/views/account/settings/AvatarModal.vue
+++ src/views/account/settings/AvatarModal.vue
@@ -130,7 +130,7 @@ export default {
           formData.append('file', data, this.fileName)
           this.$http.post('https://www.mocky.io/v2/5cc8019d300000980a055e76', formData, { contentType: false, processData: false, headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
             .then((response) => {
-              console.log("upload response:",response)
+              console.log('upload response:', response)
               // var res = response.data
               // if (response.status === 'done') {
               //   _this.imgFile = ''
