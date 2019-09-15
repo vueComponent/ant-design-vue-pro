aadb1a12d71b0b6c4689a1885c8b43cc2d4e6fb9
Sendya <18x@loacg.com>
2019-09-15 18:03:27 +0800

    chore: switch constantRouter

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,8 @@ import Vuex from 'vuex'
 import app from './modules/app'
 import user from './modules/user'
 import i18n from './modules/i18n'
-// import permission from './modules/permission'
-import permission from './modules/async-router'
+import permission from './modules/permission'
+// import permission from './modules/async-router'
 import getters from './getters'
 
 Vue.use(Vuex)
