66919b3e5cac5afb126234343eb65f26d17a32b7
Sendya <18x@loacg.com>
2019-09-15 18:02:44 +0800

    react: Login success not enter $router onComplete

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -250,12 +250,25 @@ export default {
     },
     loginSuccess (res) {
       console.log(res)
-      this.$router.push({ name: 'dashboard' }, () => {
+      // check res.homePage define, set $router.push name res.homePage
+      // Why not enter onComplete
+      /*
+      this.$router.push({ name: 'analysis' }, () => {
+        console.log('onComplete')
         this.$notification.success({
           message: '欢迎',
           description: `${timeFix()}，欢迎回来`
         })
       })
+      */
+      this.$router.push({ name: 'analysis' })
+      // 延迟 1 秒显示欢迎信息
+      setTimeout(() => {
+        this.$notification.success({
+          message: '欢迎',
+          description: `${timeFix()}，欢迎回来`
+        })
+      }, 1000)
       this.isLoginError = false
     },
     requestFailed (err) {
