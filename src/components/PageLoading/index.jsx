84fd521ee450e74fc94a43a6ee8c8da661384ba9
kokoroli <845907306@qq.com>
2019-09-24 13:41:21 +0800

    fix: pageLoading positon

--- src/components/PageLoading/index.jsx
+++ src/components/PageLoading/index.jsx
@@ -16,11 +16,11 @@ export const PageLoading = {
     const style = {
       textAlign: 'center',
       background: 'rgba(0,0,0,0.6)',
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      left: 0,
+      position: 'fixed',
       top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
       zIndex: 1100
     }
     const spinStyle = {
