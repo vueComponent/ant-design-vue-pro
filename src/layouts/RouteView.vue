c69cd9a48a4501ec127aa14950691becec0ec8f0
言肆 <18x@loacg.com>
2019-10-24 17:09:40 +0800

    Revert "fix: multi-tab with params #344"
    This reverts commit 0d37f33183b83f2bb907826a66446971e3e26109.

--- src/layouts/RouteView.vue
+++ src/layouts/RouteView.vue
@@ -14,11 +14,11 @@ export default {
     const { $route: { meta }, $store: { getters } } = this
     const inKeep = (
       <keep-alive>
-        <router-view key={ this.$route.fullPath }/>
+        <router-view />
       </keep-alive>
     )
     const notKeep = (
-      <router-view key={ this.$route.fullPath }/>
+      <router-view />
     )
     // 这里增加了 multiTab 的判断，当开启了 multiTab 时
     // 应当全部组件皆缓存，否则会导致切换页面后页面还原成原始状态
