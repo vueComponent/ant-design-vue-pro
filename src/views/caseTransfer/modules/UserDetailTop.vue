b870af2b7f9aaa5bbb3fa7040414d4958b3d0ecd
tongwudi <tongwudi@yeah.net>
2019-10-30 18:25:04 +0800

    删除注释

--- src/views/caseTransfer/modules/UserDetailTop.vue
+++ src/views/caseTransfer/modules/UserDetailTop.vue
@@ -34,76 +34,17 @@
         <p>创建时间：{{patient.createDate | moment}}</p>
       </a-col>
     </a-row>
-    <!-- <div class="userDetailContent">
-      <p class="userDetailItem">
-        <span class="userDetailItemTitle">档案编号</span>
-        <span class="userDetailItemInfo">{{patient.fileCode}}</span>
-      </p>
-      <p class="userDetailItem" style="width:10%;">
-        <span class="userDetailItemTitle">姓名</span>
-        <span class="userDetailItemInfo">{{patient.name}}</span>
-      </p>
-      <p class="userDetailItem" style="width:30%;">
-        <span class="userDetailItemTitle">身份证号</span>
-        <span class="userDetailItemInfo">{{patient.card}}</span>
-      </p>
-      <p class="userDetailItem">
-        <span class="userDetailItemTitle">联系电话</span>
-        <span class="userDetailItemInfo">{{patient.telephone}}</span>
-      </p>
-      <p class="userDetailItem">
-        <span class="userDetailItemTitle">所属中心</span>
-        <span class="userDetailItemInfo">{{patient.centerName}}</span>
-      </p>
-    </div> -->
-    <!-- <div style="margin-top:20px;">
-      <h4>申请理由：</h4>
-      <a-textarea
-        @change="handleChangeText"
-        v-model="textValue"
-        placeholder="请填写申请理由"
-        :autosize="{ minRows: 6, maxRows: 10 }"
-      />
-    </div> -->
   </div>
 </template>
 
 <script>
   import moment from 'moment'
   export default {
-    //   data() {
-    //     return {
-    //       patient: {},
-    //       textValue: ''
-    //     }
-    //   },
-    // props: {
-    //   patient: {
-    //     type: Object,
-    //     default: {}
-    //   }
-    // },
     props: ['patient'],
-    //   mounted() {
-    //     this.patient = this.userData
-    //   },
-    //   methods: {
-    //     handleChangeText() {
-    //       this.$emit('handle-change', this.textValue)
-    //     }
-    //   },
     computed: {
       getAge() {
         var identityCard = this.patient.card
         var len = (identityCard + '').length
-        if (len == 0) {
-          return 0
-        } else {
-          if (len != 15 && len != 18) {
-            //身份证号码只能为15位或18位其它不合法
-            return 0
-          }
-        }
         var strBirthday = ''
         if (len == 18) {
           //处理18位的身份证号码从号码中得到生日和性别代码
@@ -131,7 +72,6 @@
 </script>
 <style lang="less" scoped>
   .patient {
-    // border: 1px solid #f8f8f8;
     border-top: 4px solid #168ffd;
     padding: 15px 25px;
     background-color: #fff;
