038fe04ea0f9d7fac22ffb6500d53ca0cc9951d0
jslyzx <821203886@qq.com>
2019-11-07 16:34:14 +0800

    病原微生物

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -184,4 +184,28 @@ export default {
 .ant-layout-sider{
   min-height: 100vh;
 }
+.slide-right-enter-active,
+.slide-right-leave-active,
+.slide-left-enter-active,
+.slide-left-leave-active {
+  will-change: transform;
+  transition: all 500ms;
+  position: absolute;
+}
+.slide-right-enter {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.slide-left-enter {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.slide-left-leave-active {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
 </style>
