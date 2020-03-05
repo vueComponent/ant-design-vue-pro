5508838ba84f85f4a9586632b3573991fd6dc19b
EmiyaYang <1038810929@qq.com>
2020-03-05 21:51:00 +0800

    fix(menu): fail to match route item with relative path #662

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -13,14 +13,14 @@ export const asyncRouterMap = [
     children: [
       // dashboard
       {
-        path: '/dashboard',
+        path: 'dashboard',
         name: 'dashboard',
         redirect: '/dashboard/workplace',
         component: RouteView,
         meta: { title: '仪表盘', keepAlive: true, icon: bxAnaalyse, permission: [ 'dashboard' ] },
         children: [
           {
-            path: '/dashboard/analysis',
+            path: 'analysis',
             name: 'Analysis',
             component: () => import('@/views/dashboard/Analysis'),
             meta: { title: '分析页', keepAlive: false, permission: [ 'dashboard' ] }
@@ -32,13 +32,13 @@ export const asyncRouterMap = [
             meta: { title: '监控页（外部）', target: '_blank' }
           },
           {
-            path: '/dashboard/workplace',
+            path: 'workplace',
             name: 'Workplace',
             component: () => import('@/views/dashboard/Workplace'),
             meta: { title: '工作台', keepAlive: true, permission: [ 'dashboard' ] }
           },
           {
-            path: '/dashboard/test-work',
+            path: 'test-work',
             name: 'TestWork',
             component: () => import('@/views/dashboard/TestWork'),
             meta: { title: '测试功能', keepAlive: true, permission: [ 'dashboard' ] }
