91670fc511bf5aafbefb8e2aaad294b1ea81d0e1
Sendya <18x@loacg.com>
2020-08-05 16:59:41 +0800

    fix(deps): pro-layout@0.3.12 - fix contentWidth prop value type

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -33,7 +33,7 @@
 import { SettingDrawer, updateTheme } from '@ant-design-vue/pro-layout'
 import { i18nRender } from '@/locales'
 import { mapState } from 'vuex'
-import { SIDEBAR_TYPE, TOGGLE_MOBILE_TYPE } from '@/store/mutation-types'
+import { CONTENT_WIDTH_TYPE, SIDEBAR_TYPE, TOGGLE_MOBILE_TYPE } from '@/store/mutation-types'
 
 import defaultSettings from '@/config/defaultSettings'
 import RightContent from '@/components/GlobalHeader/RightContent'
@@ -63,8 +63,8 @@ export default {
       settings: {
         // 布局类型
         layout: defaultSettings.layout, // 'sidemenu', 'topmenu'
-        // 定宽: true / 流式: false
-        contentWidth: defaultSettings.layout === 'sidemenu' ? false : defaultSettings.contentWidth === 'Fixed',
+        // CONTENT_WIDTH_TYPE
+        contentWidth: defaultSettings.layout === 'sidemenu' ? CONTENT_WIDTH_TYPE.Fluid : defaultSettings.contentWidth,
         // 主题 'dark' | 'light'
         theme: defaultSettings.navTheme,
         // 主色调
@@ -128,7 +128,7 @@ export default {
       if (!this.isMobile && val['screen-xs']) {
         this.isMobile = true
         this.collapsed = false
-        this.settings.contentWidth = false
+        this.settings.contentWidth = CONTENT_WIDTH_TYPE.Fluid
         // this.settings.fixSiderbar = false
       }
     },
@@ -140,14 +140,14 @@ export default {
       type && (this.settings[type] = value)
       switch (type) {
         case 'contentWidth':
-          this.settings[type] = value === 'Fixed'
+          this.settings[type] = value
           break
         case 'layout':
           if (value === 'sidemenu') {
-            this.settings.contentWidth = false
+            this.settings.contentWidth = CONTENT_WIDTH_TYPE.Fluid
           } else {
             this.settings.fixSiderbar = false
-            this.settings.contentWidth = true
+            this.settings.contentWidth = CONTENT_WIDTH_TYPE.Fixed
           }
           break
       }
