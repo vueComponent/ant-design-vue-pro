25af5cc689e08b40baa658bd4d7b4a45491cbd28
Mandeep <msg8930@yahoo.com>
2020-12-04 22:21:14 +0300

    refactor form menu title form hard code to locale

--- src/locales/lang/en-US/menu.js
+++ src/locales/lang/en-US/menu.js
@@ -4,5 +4,12 @@ export default {
     'menu.dashboard': 'Dashboard',
     'menu.dashboard.analysis': 'Analysis',
     'menu.dashboard.monitor': 'Monitor',
-    'menu.dashboard.workplace': 'Workplace'
+    'menu.dashboard.workplace': 'Workplace',
+    'menu.form': 'Form',
+    'menu.form.basic-form': 'Basic Form',
+    'menu.form.step-form': 'Step Form',
+    'menu.form.step-form.info': 'Step Form(write transfer information)',
+    'menu.form.step-form.confirm': 'Step Form(confirm transfer information)',
+    'menu.form.step-form.result': 'Step Form(finished)',
+    'menu.form.advanced-form': 'Advanced Form'
 }
--- src/locales/lang/zh-CN/menu.js
+++ src/locales/lang/zh-CN/menu.js
@@ -4,5 +4,12 @@ export default {
     'menu.dashboard': '仪表盘',
     'menu.dashboard.analysis': '分析页',
     'menu.dashboard.monitor': '监控页',
-    'menu.dashboard.workplace': '工作台'
+    'menu.dashboard.workplace': '工作台',
+    'menu.form': '表单页',
+    'menu.form.basic-form': '基础表单',
+    'menu.form.step-form': '分步表单',
+    'menu.form.step-form.info': '分步表单（填写转账信息）',
+    'menu.form.step-form.confirm': '分步表单（确认转账信息）',
+    'menu.form.step-form.result': '分步表单（完成）',
+    'menu.form.advanced-form': '高级表单'
 }
