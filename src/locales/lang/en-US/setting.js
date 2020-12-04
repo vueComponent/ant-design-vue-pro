0fbd79134b24db68bec3ca5936c25233f803a26f
Mandeep <msg8930@yahoo.com>
2020-12-04 21:39:13 +0300

    add en-US directory  to the locales directory and move the code from en-US.js file to different files

---
+++ src/locales/lang/en-US/setting.js
@@ -0,0 +1,22 @@
+export default {
+    'app.setting.pagestyle': 'Page style setting',
+    'app.setting.pagestyle.light': 'Light style',
+    'app.setting.pagestyle.dark': 'Dark style',
+    'app.setting.pagestyle.realdark': 'RealDark style',
+    'app.setting.themecolor': 'Theme Color',
+    'app.setting.navigationmode': 'Navigation Mode',
+    'app.setting.content-width': 'Content Width',
+    'app.setting.fixedheader': 'Fixed Header',
+    'app.setting.fixedsidebar': 'Fixed Sidebar',
+    'app.setting.sidemenu': 'Side Menu Layout',
+    'app.setting.topmenu': 'Top Menu Layout',
+    'app.setting.content-width.fixed': 'Fixed',
+    'app.setting.content-width.fluid': 'Fluid',
+    'app.setting.othersettings': 'Other Settings',
+    'app.setting.weakmode': 'Weak Mode',
+    'app.setting.copy': 'Copy Setting',
+    'app.setting.loading': 'Loading theme',
+    'app.setting.copyinfo': 'copy success，please replace defaultSettings in src/models/setting.js',
+    'app.setting.production.hint': 'Setting panel shows in development environment only, please manually modify'
+
+}
