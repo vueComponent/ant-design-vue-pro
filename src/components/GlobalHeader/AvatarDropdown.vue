9f4a28a380eb669ea25639ff65409fda525e5bda
Mandeep <msg8930@yahoo.com>
2020-12-05 01:04:29 +0300

    refactor AvtarDropdown hardcoded menu options to the language support

--- src/components/GlobalHeader/AvatarDropdown.vue
+++ src/components/GlobalHeader/AvatarDropdown.vue
@@ -8,16 +8,16 @@
       <a-menu class="ant-pro-drop-down menu" :selected-keys="[]">
         <a-menu-item v-if="menu" key="center" @click="handleToCenter">
           <a-icon type="user" />
-          个人中心
+          {{ $t('menu.account.center') }}
         </a-menu-item>
         <a-menu-item v-if="menu" key="settings" @click="handleToSettings">
           <a-icon type="setting" />
-          个人设置
+          {{ $t('menu.account.settings') }}
         </a-menu-item>
         <a-menu-divider v-if="menu" />
         <a-menu-item key="logout" @click="handleLogout">
           <a-icon type="logout" />
-          退出登录
+          {{ $t('menu.account.logout') }}
         </a-menu-item>
       </a-menu>
     </template>
