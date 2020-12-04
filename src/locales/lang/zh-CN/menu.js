58f5b3fb897766fa2b65a754fd3d849b596860ea
Mandeep <msg8930@yahoo.com>
2020-12-04 22:58:58 +0300

    refactor Menu Result and Exception from hard-coded to language support

--- src/locales/lang/zh-CN/menu.js
+++ src/locales/lang/zh-CN/menu.js
@@ -22,5 +22,13 @@ export default {
     'menu.list.search-list.applications': '搜索列表（应用）',
     'menu.profile': '详情页',
     'menu.profile.basic': '基础详情页',
-    'menu.profile.advanced': '高级详情页'
+    'menu.profile.advanced': '高级详情页',
+    'menu.result': '结果页',
+    'menu.result.success': '成功页',
+    'menu.result.fail': '失败页',
+    'menu.exception': '异常页',
+    'menu.exception.not-permission': '403',
+    'menu.exception.not-find': '404',
+    'menu.exception.server-error': '500',
+    'menu.exception.trigger': '触发错误'
 }
