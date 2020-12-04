5dc171256ab4394ca90f37c076beabf042d32d6f
Mandeep <msg8930@yahoo.com>
2020-12-04 21:39:13 +0300

    add en-US directory  to the locales directory and move the code from en-US.js file to different files

---
+++ src/locales/lang/en-US/menu.js
@@ -0,0 +1,8 @@
+export default {
+    'menu.welcome': 'Welcome',
+    'menu.home': 'Home',
+    'menu.dashboard': 'Dashboard',
+    'menu.dashboard.analysis': 'Analysis',
+    'menu.dashboard.monitor': 'Monitor',
+    'menu.dashboard.workplace': 'Workplace'
+}
