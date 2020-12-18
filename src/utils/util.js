5dac321541252ba10fd056c89a30a82818d0ef20
Mandeep <msg8930@yahoo.com>
2020-12-19 02:48:16 +0300

    fixed register password inline validation

--- src/utils/util.js
+++ src/utils/util.js
@@ -65,3 +65,31 @@ export function removeLoadingAnimate (id = '', timeout = 1500) {
     document.body.removeChild(document.getElementById(id))
   }, timeout)
 }
+export function scorePassword (pass) {
+  let score = 0
+  if (!pass) {
+    return score
+  }
+  // award every unique letter until 5 repetitions
+  const letters = {}
+  for (let i = 0; i < pass.length; i++) {
+      letters[pass[i]] = (letters[pass[i]] || 0) + 1
+      score += 5.0 / letters[pass[i]]
+  }
+
+  // bonus points for mixing it up
+  const variations = {
+      digits: /\d/.test(pass),
+      lower: /[a-z]/.test(pass),
+      upper: /[A-Z]/.test(pass),
+      nonWords: /\W/.test(pass)
+  }
+
+  let variationCount = 0
+  for (var check in variations) {
+      variationCount += (variations[check] === true) ? 1 : 0
+  }
+  score += (variationCount - 1) * 10
+
+  return parseInt(score)
+}
