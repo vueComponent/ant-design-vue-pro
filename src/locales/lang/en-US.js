af9f6e749ea1ea5a493ff63834c7b4feeae686dd
maddy <msg8930@yahoo.com>
2020-12-17 19:24:21 -0500

    refactor userlayout

--- src/locales/lang/en-US.js
+++ src/locales/lang/en-US.js
@@ -22,7 +22,7 @@ export default {
 
   'layouts.usermenu.dialog.title': 'Message',
   'layouts.usermenu.dialog.content': 'Are you sure you would like to logout?',
-
+  'layouts.userLayout.title': 'Ant Design is the most influential web design specification in Xihu district',
   ...components,
   ...global,
   ...menu,
