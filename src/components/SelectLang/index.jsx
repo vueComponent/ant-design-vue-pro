e7654991a9cb545a790192c32c9395dfddb7e19f
AnsonZnl <1160030724@qq.com>
2021-07-16 15:52:25 +0800

    chore: cleanup

--- src/components/SelectLang/index.jsx
+++ src/components/SelectLang/index.jsx
@@ -4,19 +4,15 @@ import { Icon, Menu, Dropdown } from 'ant-design-vue'
 import { i18nRender } from '@/locales'
 import i18nMixin from '@/store/i18n-mixin'
 
-const locales = ['zh-CN', 'zh-TW', 'en-US', 'pt-BR']
+const locales = ['zh-CN', 'en-US']
 const languageLabels = {
   'zh-CN': '简体中文',
-  'zh-TW': '繁体中文',
-  'en-US': 'English',
-  'pt-BR': 'Português'
+  'en-US': 'English'
 }
 // eslint-disable-next-line
 const languageIcons = {
   'zh-CN': '🇨🇳',
-  'zh-TW': '🇭🇰',
-  'en-US': '🇺🇸',
-  'pt-BR': '🇧🇷'
+  'en-US': '🇺🇸'
 }
 
 const SelectLang = {
