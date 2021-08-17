e30976b9058240d90887e204273713bbed6e1508
sam <sammeishi@users.noreply.github.com>
2021-08-17 15:47:14 +0800

    fix: bug 修复打包后报错 TypeError: le.addRoute is not a function
    原因是本地开发阶段引用的是npm包中的vue-router，与打包后externals中CDN载入的vue-router版本不一致导致的。

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,7 @@ const assetsCDN = {
   // https://unpkg.com/browse/vue@2.6.10/
   js: [
     '//cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js',
-    '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
+    '//cdn.jsdelivr.net/npm/vue-router@3.5.1/dist/vue-router.min.js',
     '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
     '//cdn.jsdelivr.net/npm/axios@0.21.1/dist/axios.min.js'
   ]
