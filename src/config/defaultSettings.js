8bfc899ea2ae2c167160666d00a16f8365076a9d
Yulong Li <1374803733@qq.com>
2021-08-17 14:38:43 +0800

    router测试

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -23,7 +23,7 @@ export default {
   menu: {
     locale: true
   },
-  title: 'Ant Design Pro',
+  title: '恒星号运营中台',
   pwa: false,
   iconfontUrl: '',
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true'
--- src/mock/services/user.js
+++ src/mock/services/user.js
@@ -474,7 +474,7 @@ const userNav = options => {
       id: 1,
       meta: {
         icon: 'dashboard',
-        title: '仪表盘',
+        title: '账号管理',
         show: true
       },
       component: 'RouteView',
@@ -485,7 +485,7 @@ const userNav = options => {
       parentId: 1,
       id: 7,
       meta: {
-        title: '工作台',
+        title: '行星号',
         show: true
       },
       component: 'Workplace'
@@ -496,7 +496,7 @@ const userNav = options => {
       parentId: 1,
       id: 3,
       meta: {
-        title: '监控页（外部）',
+        title: '恒星号',
         target: '_blank',
         show: true
       }
@@ -506,7 +506,7 @@ const userNav = options => {
       parentId: 1,
       id: 2,
       meta: {
-        title: '分析页',
+        title: '恒星号创建',
         show: true
       },
       component: 'Analysis',
@@ -520,7 +520,7 @@ const userNav = options => {
       id: 10,
       meta: {
         icon: 'form',
-        title: '表单页'
+        title: '动态管理'
       },
       redirect: '/form/base-form',
       component: 'RouteView'
@@ -530,7 +530,7 @@ const userNav = options => {
       parentId: 10,
       id: 6,
       meta: {
-        title: '基础表单'
+        title: '济事动态'
       },
       component: 'BasicForm'
     },
@@ -539,18 +539,9 @@ const userNav = options => {
       parentId: 10,
       id: 5,
       meta: {
-        title: '分步表单'
+        title: '济人动态'
       },
       component: 'StepForm'
-    },
-    {
-      name: 'advanced-form',
-      parentId: 10,
-      id: 4,
-      meta: {
-        title: '高级表单'
-      },
-      component: 'AdvanceForm'
     },
 
     // list
@@ -560,162 +551,45 @@ const userNav = options => {
       id: 10010,
       meta: {
         icon: 'table',
-        title: '列表页',
+        title: '济人动态发布',
         show: true
       },
       redirect: '/list/table-list',
       component: 'RouteView'
     },
-    {
-      name: 'table-list',
-      parentId: 10010,
-      id: 10011,
-      path: '/list/table-list/:pageNo([1-9]\\d*)?',
-      meta: {
-        title: '查询表格',
-        show: true
-      },
-      component: 'TableList'
-    },
-    {
-      name: 'basic-list',
-      parentId: 10010,
-      id: 10012,
-      meta: {
-        title: '标准列表',
-        show: true
-      },
-      component: 'StandardList'
-    },
-    {
-      name: 'card',
-      parentId: 10010,
-      id: 10013,
-      meta: {
-        title: '卡片列表',
-        show: true
-      },
-      component: 'CardList'
-    },
-    {
-      name: 'search',
-      parentId: 10010,
-      id: 10014,
-      meta: {
-        title: '搜索列表',
-        show: true
-      },
-      redirect: '/list/search/article',
-      component: 'SearchLayout'
-    },
-    {
-      name: 'article',
-      parentId: 10014,
-      id: 10015,
-      meta: {
-        title: '搜索列表（文章）',
-        show: true
-      },
-      component: 'SearchArticles'
-    },
-    {
-      name: 'project',
-      parentId: 10014,
-      id: 10016,
-      meta: {
-        title: '搜索列表（项目）',
-        show: true
-      },
-      component: 'SearchProjects'
-    },
-    {
-      name: 'application',
-      parentId: 10014,
-      id: 10017,
-      meta: {
-        title: '搜索列表（应用）',
-        show: true
-      },
-      component: 'SearchApplications'
-    },
-
     // profile
     {
       name: 'profile',
       parentId: 0,
       id: 10018,
       meta: {
-        title: '详情页',
+        title: '用户反馈',
         icon: 'profile',
         show: true
       },
       redirect: '/profile/basic',
       component: 'RouteView'
     },
-    {
-      name: 'basic',
-      parentId: 10018,
-      id: 10019,
-      meta: {
-        title: '基础详情页',
-        show: true
-      },
-      component: 'ProfileBasic'
-    },
-    {
-      name: 'advanced',
-      parentId: 10018,
-      id: 10020,
-      meta: {
-        title: '高级详情页',
-        show: true
-      },
-      component: 'ProfileAdvanced'
-    },
-
     // result
     {
       name: 'result',
       parentId: 0,
       id: 10021,
       meta: {
-        title: '结果页',
+        title: '数据需求',
         icon: 'check-circle-o',
         show: true
       },
       redirect: '/result/success',
       component: 'PageView'
     },
-    {
-      name: 'success',
-      parentId: 10021,
-      id: 10022,
-      meta: {
-        title: '成功',
-        hiddenHeaderContent: true,
-        show: true
-      },
-      component: 'ResultSuccess'
-    },
-    {
-      name: 'fail',
-      parentId: 10021,
-      id: 10023,
-      meta: {
-        title: '失败',
-        hiddenHeaderContent: true,
-        show: true
-      },
-      component: 'ResultFail'
-    },
-
     // Exception
     {
       name: 'exception',
       parentId: 0,
       id: 10024,
       meta: {
-        title: '异常页',
+        title: '审核济事',
         icon: 'warning',
         show: true
       },
@@ -727,7 +601,7 @@ const userNav = options => {
       parentId: 10024,
       id: 10025,
       meta: {
-        title: '403',
+        title: '审核信息',
         show: true
       },
       component: 'Exception403'
@@ -737,29 +611,18 @@ const userNav = options => {
       parentId: 10024,
       id: 10026,
       meta: {
-        title: '404',
+        title: '帖子筛选',
         show: true
       },
       component: 'Exception404'
     },
-    {
-      name: '500',
-      parentId: 10024,
-      id: 10027,
-      meta: {
-        title: '500',
-        show: true
-      },
-      component: 'Exception500'
-    },
-
     // account
     {
       name: 'account',
       parentId: 0,
       id: 10028,
       meta: {
-        title: '个人页',
+        title: '审核组队',
         icon: 'user',
         show: true
       },
@@ -771,85 +634,423 @@ const userNav = options => {
       parentId: 10028,
       id: 10029,
       meta: {
-        title: '个人中心',
+        title: '审核信息',
         show: true
       },
       component: 'AccountCenter'
     },
-    // 特殊三级菜单
     {
-      name: 'settings',
+      name: 'center',
       parentId: 10028,
-      id: 10030,
+      id: 10029,
       meta: {
-        title: '个人设置',
-        hideHeader: true,
-        hideChildren: true,
+        title: '帖子筛选',
         show: true
       },
-      redirect: '/account/settings/base',
-      component: 'AccountSettings'
+      component: 'AccountCenter'
     },
     {
-      name: 'BasicSetting',
-      path: '/account/settings/base',
-      parentId: 10030,
-      id: 10031,
+      name: 'center',
+      parentId: 0,
+      id: 10029,
       meta: {
-        title: '基本设置',
-        show: false
+        title: '审核管理(进度查看)',
+        show: true,
+        icon: 'branches'
       },
-      component: 'BasicSetting'
-    },
-    {
-      name: 'SecuritySettings',
-      path: '/account/settings/security',
-      parentId: 10030,
-      id: 10032,
-      meta: {
-        title: '安全设置',
-        show: false
-      },
-      component: 'SecuritySettings'
-    },
-    {
-      name: 'CustomSettings',
-      path: '/account/settings/custom',
-      parentId: 10030,
-      id: 10033,
-      meta: {
-        title: '个性化设置',
-        show: false
-      },
-      component: 'CustomSettings'
-    },
-    {
-      name: 'BindingSettings',
-      path: '/account/settings/binding',
-      parentId: 10030,
-      id: 10034,
-      meta: {
-        title: '账户绑定',
-        show: false
-      },
-      component: 'BindingSetting'
-    },
-    {
-      name: 'NotificationSettings',
-      path: '/account/settings/notification',
-      parentId: 10030,
-      id: 10034,
-      meta: {
-        title: '新消息通知',
-        show: false
-      },
-      component: 'NotificationSettings'
+      component: 'AccountCenter'
     }
   ]
   const json = builder(nav)
   console.log('json', json)
   return json
 }
+
+// const userNav = options => {
+//   const nav = [
+//     // dashboard
+//     {
+//       name: 'dashboard',
+//       parentId: 0,
+//       id: 1,
+//       meta: {
+//         icon: 'dashboard',
+//         title: '仪表盘',
+//         show: true
+//       },
+//       component: 'RouteView',
+//       redirect: '/dashboard/workplace'
+//     },
+//     {
+//       name: 'workplace',
+//       parentId: 1,
+//       id: 7,
+//       meta: {
+//         title: '工作台',
+//         show: true
+//       },
+//       component: 'Workplace'
+//     },
+//     {
+//       name: 'monitor',
+//       path: 'https://www.baidu.com/',
+//       parentId: 1,
+//       id: 3,
+//       meta: {
+//         title: '监控页（外部）',
+//         target: '_blank',
+//         show: true
+//       }
+//     },
+//     {
+//       name: 'Analysis',
+//       parentId: 1,
+//       id: 2,
+//       meta: {
+//         title: '分析页',
+//         show: true
+//       },
+//       component: 'Analysis',
+//       path: '/dashboard/analysis'
+//     },
+//
+//     // form
+//     {
+//       name: 'form',
+//       parentId: 0,
+//       id: 10,
+//       meta: {
+//         icon: 'form',
+//         title: '表单页'
+//       },
+//       redirect: '/form/base-form',
+//       component: 'RouteView'
+//     },
+//     {
+//       name: 'basic-form',
+//       parentId: 10,
+//       id: 6,
+//       meta: {
+//         title: '基础表单'
+//       },
+//       component: 'BasicForm'
+//     },
+//     {
+//       name: 'step-form',
+//       parentId: 10,
+//       id: 5,
+//       meta: {
+//         title: '分步表单'
+//       },
+//       component: 'StepForm'
+//     },
+//     {
+//       name: 'advanced-form',
+//       parentId: 10,
+//       id: 4,
+//       meta: {
+//         title: '高级表单'
+//       },
+//       component: 'AdvanceForm'
+//     },
+//
+//     // list
+//     {
+//       name: 'list',
+//       parentId: 0,
+//       id: 10010,
+//       meta: {
+//         icon: 'table',
+//         title: '列表页',
+//         show: true
+//       },
+//       redirect: '/list/table-list',
+//       component: 'RouteView'
+//     },
+//     {
+//       name: 'table-list',
+//       parentId: 10010,
+//       id: 10011,
+//       path: '/list/table-list/:pageNo([1-9]\\d*)?',
+//       meta: {
+//         title: '查询表格',
+//         show: true
+//       },
+//       component: 'TableList'
+//     },
+//     {
+//       name: 'basic-list',
+//       parentId: 10010,
+//       id: 10012,
+//       meta: {
+//         title: '标准列表',
+//         show: true
+//       },
+//       component: 'StandardList'
+//     },
+//     {
+//       name: 'card',
+//       parentId: 10010,
+//       id: 10013,
+//       meta: {
+//         title: '卡片列表',
+//         show: true
+//       },
+//       component: 'CardList'
+//     },
+//     {
+//       name: 'search',
+//       parentId: 10010,
+//       id: 10014,
+//       meta: {
+//         title: '搜索列表',
+//         show: true
+//       },
+//       redirect: '/list/search/article',
+//       component: 'SearchLayout'
+//     },
+//     {
+//       name: 'article',
+//       parentId: 10014,
+//       id: 10015,
+//       meta: {
+//         title: '搜索列表（文章）',
+//         show: true
+//       },
+//       component: 'SearchArticles'
+//     },
+//     {
+//       name: 'project',
+//       parentId: 10014,
+//       id: 10016,
+//       meta: {
+//         title: '搜索列表（项目）',
+//         show: true
+//       },
+//       component: 'SearchProjects'
+//     },
+//     {
+//       name: 'application',
+//       parentId: 10014,
+//       id: 10017,
+//       meta: {
+//         title: '搜索列表（应用）',
+//         show: true
+//       },
+//       component: 'SearchApplications'
+//     },
+//
+//     // profile
+//     {
+//       name: 'profile',
+//       parentId: 0,
+//       id: 10018,
+//       meta: {
+//         title: '详情页',
+//         icon: 'profile',
+//         show: true
+//       },
+//       redirect: '/profile/basic',
+//       component: 'RouteView'
+//     },
+//     {
+//       name: 'basic',
+//       parentId: 10018,
+//       id: 10019,
+//       meta: {
+//         title: '基础详情页',
+//         show: true
+//       },
+//       component: 'ProfileBasic'
+//     },
+//     {
+//       name: 'advanced',
+//       parentId: 10018,
+//       id: 10020,
+//       meta: {
+//         title: '高级详情页',
+//         show: true
+//       },
+//       component: 'ProfileAdvanced'
+//     },
+//
+//     // result
+//     {
+//       name: 'result',
+//       parentId: 0,
+//       id: 10021,
+//       meta: {
+//         title: '结果页',
+//         icon: 'check-circle-o',
+//         show: true
+//       },
+//       redirect: '/result/success',
+//       component: 'PageView'
+//     },
+//     {
+//       name: 'success',
+//       parentId: 10021,
+//       id: 10022,
+//       meta: {
+//         title: '成功',
+//         hiddenHeaderContent: true,
+//         show: true
+//       },
+//       component: 'ResultSuccess'
+//     },
+//     {
+//       name: 'fail',
+//       parentId: 10021,
+//       id: 10023,
+//       meta: {
+//         title: '失败',
+//         hiddenHeaderContent: true,
+//         show: true
+//       },
+//       component: 'ResultFail'
+//     },
+//
+//     // Exception
+//     {
+//       name: 'exception',
+//       parentId: 0,
+//       id: 10024,
+//       meta: {
+//         title: '异常页',
+//         icon: 'warning',
+//         show: true
+//       },
+//       redirect: '/exception/403',
+//       component: 'RouteView'
+//     },
+//     {
+//       name: '403',
+//       parentId: 10024,
+//       id: 10025,
+//       meta: {
+//         title: '403',
+//         show: true
+//       },
+//       component: 'Exception403'
+//     },
+//     {
+//       name: '404',
+//       parentId: 10024,
+//       id: 10026,
+//       meta: {
+//         title: '404',
+//         show: true
+//       },
+//       component: 'Exception404'
+//     },
+//     {
+//       name: '500',
+//       parentId: 10024,
+//       id: 10027,
+//       meta: {
+//         title: '500',
+//         show: true
+//       },
+//       component: 'Exception500'
+//     },
+//
+//     // account
+//     {
+//       name: 'account',
+//       parentId: 0,
+//       id: 10028,
+//       meta: {
+//         title: '个人页',
+//         icon: 'user',
+//         show: true
+//       },
+//       redirect: '/account/center',
+//       component: 'RouteView'
+//     },
+//     {
+//       name: 'center',
+//       parentId: 10028,
+//       id: 10029,
+//       meta: {
+//         title: '个人中心',
+//         show: true
+//       },
+//       component: 'AccountCenter'
+//     },
+//     // 特殊三级菜单
+//     {
+//       name: 'settings',
+//       parentId: 10028,
+//       id: 10030,
+//       meta: {
+//         title: '个人设置',
+//         hideHeader: true,
+//         hideChildren: true,
+//         show: true
+//       },
+//       redirect: '/account/settings/base',
+//       component: 'AccountSettings'
+//     },
+//     {
+//       name: 'BasicSetting',
+//       path: '/account/settings/base',
+//       parentId: 10030,
+//       id: 10031,
+//       meta: {
+//         title: '基本设置',
+//         show: false
+//       },
+//       component: 'BasicSetting'
+//     },
+//     {
+//       name: 'SecuritySettings',
+//       path: '/account/settings/security',
+//       parentId: 10030,
+//       id: 10032,
+//       meta: {
+//         title: '安全设置',
+//         show: false
+//       },
+//       component: 'SecuritySettings'
+//     },
+//     {
+//       name: 'CustomSettings',
+//       path: '/account/settings/custom',
+//       parentId: 10030,
+//       id: 10033,
+//       meta: {
+//         title: '个性化设置',
+//         show: false
+//       },
+//       component: 'CustomSettings'
+//     },
+//     {
+//       name: 'BindingSettings',
+//       path: '/account/settings/binding',
+//       parentId: 10030,
+//       id: 10034,
+//       meta: {
+//         title: '账户绑定',
+//         show: false
+//       },
+//       component: 'BindingSetting'
+//     },
+//     {
+//       name: 'NotificationSettings',
+//       path: '/account/settings/notification',
+//       parentId: 10030,
+//       id: 10034,
+//       meta: {
+//         title: '新消息通知',
+//         show: false
+//       },
+//       component: 'NotificationSettings'
+//     }
+//   ]
+//   const json = builder(nav)
+//   console.log('json', json)
+//   return json
+// }
 
 Mock.mock(/\/api\/user\/info/, 'get', info)
 Mock.mock(/\/api\/user\/nav/, 'get', userNav)
