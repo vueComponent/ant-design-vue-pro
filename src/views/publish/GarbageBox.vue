ffc84ef6b7682068261f051a355a1ff02b11c587
Wei-Xiaoxing <281796586@qq.com>
2021-08-24 14:02:15 +0800

    制作运营动态的发布管理页面的三个界面
    在原有已发布区域的基础上，增添了草稿箱和垃圾箱，并且将这三个界面分别写成了组件，一同放在publish文件夹下。

---
+++ src/views/publish/GarbageBox.vue
@@ -0,0 +1,202 @@
+<template>
+  <div>
+    <a-form layout='inline' :form='form' @submit='handleSubmit'>
+      <a-row :gutter='24'>
+        <a-col :span='2'>
+          <a-button type='primary'>刷新</a-button>
+        </a-col>
+        <a-col :span='3'>
+          <a-form-item label='动态ID'>
+            <a-input v-decorator='["id"]'/>
+          </a-form-item>
+        </a-col>
+        <a-col :span='3'>
+          <a-form-item label='发布人'>
+            <a-select v-decorator='["publisher"]'>
+            </a-select>
+          </a-form-item>
+        </a-col>
+        <a-col :span='3'>
+          <a-form-item label='板块'>
+            <a-select v-decorator='["plate"]'>
+              <a-select-option value='0'>济事</a-select-option>
+              <a-select-option value='1'>济人</a-select-option>
+              <a-select-option value='2'>同德</a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-col>
+        <a-col :span='3'>
+          <a-form-item label='主题'>
+            <a-select v-decorator='["theme"]'>
+              <a-select-option value='0'>求职信息</a-select-option>
+              <a-select-option value='1'>学习天地</a-select-option>
+              <a-select-option value='2'>校园活动</a-select-option>
+              <a-select-option value='3'>生活指南</a-select-option>
+              <a-select-option value='4'>竞赛</a-select-option>
+              <a-select-option value='5'>学术科研</a-select-option>
+              <a-select-option value='6'>一起造梦</a-select-option>
+              <a-select-option value='7'>其他</a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-col>
+        <a-col :span='3'>
+          <a-form-item>
+            <a-date-picker v-decorator='["beginTime"]'/>
+          </a-form-item>
+        </a-col>
+        <a-col :span='3'>
+          <a-form-item>
+            <a-date-picker v-decorator='["endTime"]'/>
+          </a-form-item>
+        </a-col>
+        <a-col :span='1'>
+          <a-button type='primary' html-type='submit'>筛选</a-button>
+        </a-col>
+        <a-col :span='1' :offset='1'>
+          <a-button type='primary'>重置</a-button>
+        </a-col>
+      </a-row>
+    </a-form>
+
+    <a-table :columns='columns' :data-source='data'>
+        <span slot='operations'>
+          <a>恢复 </a>
+          <a>修改 </a>
+          <a>删除</a>
+        </span>
+    </a-table>
+  </div>
+</template>
+
+<script>
+const columns = [
+  {
+    title: '动态ID',
+    dataIndex: 'ID',
+    key: 'ID'
+  },
+  {
+    title: '发布人',
+    dataIndex: 'publisher',
+    key: 'publisher'
+  },
+  {
+    title: '板块',
+    dataIndex: 'plane',
+    key: 'plane'
+  },
+  {
+    title: '主题',
+    dataIndex: 'theme',
+    key: 'theme'
+  },
+  {
+    title: '发布时间',
+    dataIndex: 'publishTime',
+    key: 'publishTime'
+  },
+  {
+    title: '标题',
+    dataIndex: 'title',
+    key: 'title',
+    ellipsis: true
+  },
+  {
+    title: '主要内容',
+    dataIndex: 'content',
+    key: 'content',
+    ellipsis: true
+  },
+  {
+    title: '操作',
+    dataIndex: 'operations',
+    key: 'operations',
+    scopedSlots: { customRender: 'operations' }
+  }
+]
+
+const data = [
+  {
+    key: '1',
+    ID: '1234',
+    publisher: '胡彦斌',
+    plane: '济人',
+    theme: '校园活动',
+    publishTime: '2017-10-01 12:00',
+    title: '关于新学期的一些和新学期有关的',
+    content: '同学们好，新学期来了，久学期过去了。'
+  }, {
+    key: '2',
+    ID: '1234',
+    publisher: '胡彦斌',
+    plane: '济人',
+    theme: '求职之路',
+    publishTime: '2017-10-01 12:00',
+    title: '关于新学期的一些和新学期有关的',
+    content: '同学们好，新学期来了，久学期过去了。'
+  }, {
+    key: '3',
+    ID: '1234',
+    publisher: '胡彦斌',
+    plane: '济事',
+    theme: '校园活动',
+    publishTime: '2017-10-01 12:00',
+    title: '关于新学期的一些和新学期有关的',
+    content: '同学们好，新学期来了，久学期过去了。'
+  }, {
+    key: '4',
+    ID: '1234',
+    publisher: '胡彦斌',
+    plane: '济事',
+    theme: '生活指南',
+    publishTime: '2017-10-01 12:00',
+    title: '关于新学期的一些和新学期有关的',
+    content: '同学们好，新学期来了，久学期过去了。'
+  }, {
+    key: '5',
+    ID: '1234',
+    publisher: '胡彦斌',
+    plane: '同德',
+    theme: '学习天地',
+    publishTime: '2017-10-01 12:00',
+    title: '关于新学期的一些和新学期有关的',
+    content: '同学们好，新学期来了，久学期过去了。'
+  }, {
+    key: '6',
+    ID: '1234',
+    publisher: '胡彦斌',
+    plane: '同德',
+    theme: '学习天地',
+    publishTime: '2017-10-01 12:00',
+    title: '关于新学期的一些和新学期有关的',
+    content: '同学们好，新学期来了，久学期过去了。'
+  }
+]
+
+export default {
+  name: 'GarbageBox',
+  data () {
+    return {
+      columns,
+      data
+    }
+  },
+  beforeCreate () {
+    this.form = this.$form.createForm(this, { name: 'search' })
+  },
+  methods: {
+    handleSubmit (e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log(values)
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/publish/PublishManagement.vue
+++ src/views/publish/PublishManagement.vue
@@ -3,205 +3,30 @@
     <a-card :bordered="false">
       <div class="table-page-search-wrapper">
         <a-row :gutter='96'>
-          <a-col :span='1'><a-button type='primary'>已发布</a-button> </a-col>
-          <a-col :span='1'><a-button>草稿箱</a-button></a-col>
-          <a-col :span='1'><a-button>垃圾箱</a-button></a-col>
+          <a-col :span='1'><a-button :type='this.interface=="published"?"primary":"default"' @click='() => this.interface="published"'>已发布</a-button> </a-col>
+          <a-col :span='1'><a-button :type='this.interface=="draft"?"primary":"default"'@click='() => this.interface="draft"'>草稿箱</a-button></a-col>
+          <a-col :span='1'><a-button :type='this.interface=="garbage"?"primary":"default"'@click='() => this.interface="garbage"'>垃圾箱</a-button></a-col>
         </a-row>
         <div style='height: 20px'/>
-        <a-form layout='inline' :form='form' @submit='handleSubmit'>
-          <a-row :gutter='24'>
-            <a-col :span='2'>
-              <a-button type='primary'>刷新</a-button>
-            </a-col>
-            <a-col :span='3'>
-              <a-form-item label='动态ID'>
-                <a-input v-decorator='["id"]'/>
-              </a-form-item>
-            </a-col>
-            <a-col :span='3'>
-              <a-form-item label='发布人'>
-                <a-select v-decorator='["publisher"]'>
-                </a-select>
-              </a-form-item>
-            </a-col>
-            <a-col :span='3'>
-              <a-form-item label='板块'>
-                <a-select v-decorator='["plate"]'>
-                  <a-select-option value='0'>济事</a-select-option>
-                  <a-select-option value='1'>济人</a-select-option>
-                  <a-select-option value='2'>同德</a-select-option>
-                </a-select>
-              </a-form-item>
-            </a-col>
-            <a-col :span='3'>
-              <a-form-item label='主题'>
-                <a-select v-decorator='["theme"]'>
-                  <a-select-option value='0'>求职信息</a-select-option>
-                  <a-select-option value='1'>学习天地</a-select-option>
-                  <a-select-option value='2'>校园活动</a-select-option>
-                  <a-select-option value='3'>生活指南</a-select-option>
-                  <a-select-option value='4'>竞赛</a-select-option>
-                  <a-select-option value='5'>学术科研</a-select-option>
-                  <a-select-option value='6'>一起造梦</a-select-option>
-                  <a-select-option value='7'>其他</a-select-option>
-                </a-select>
-              </a-form-item>
-            </a-col>
-            <a-col :span='3'>
-              <a-form-item>
-                <a-date-picker v-decorator='["beginTime"]'/>
-              </a-form-item>
-            </a-col>
-            <a-col :span='3'>
-              <a-form-item>
-                <a-date-picker v-decorator='["endTime"]'/>
-              </a-form-item>
-            </a-col>
-            <a-col :span='1'>
-              <a-button type='primary' html-type='submit'>筛选</a-button>
-            </a-col>
-            <a-col :span='1' :offset='1'>
-              <a-button type='primary'>重置</a-button>
-            </a-col>
-          </a-row>
-        </a-form>
+        <PublishedBox v-show='this.interface=="published"'></PublishedBox>
+        <DraftBox v-show='this.interface=="draft"'></DraftBox>
+        <GarbageBox v-show='this.interface=="garbage"'></GarbageBox>
       </div>
-
-      <a-table :columns='columns' :data-source='data'>
-        <span slot='operations'>
-          <a>查看原帖 </a>
-          <a>修改 </a>
-          <a>删除</a>
-        </span>
-      </a-table>
     </a-card>
   </page-header-wrapper>
 </template>
 
 <script>
-
-const columns = [
-  {
-    title: '动态ID',
-    dataIndex: 'ID',
-    key: 'ID'
-  },
-  {
-    title: '发布人',
-    dataIndex: 'publisher',
-    key: 'publisher'
-  },
-  {
-    title: '板块',
-    dataIndex: 'plane',
-    key: 'plane'
-  },
-  {
-    title: '主题',
-    dataIndex: 'theme',
-    key: 'theme'
-  },
-  {
-    title: '发布时间',
-    dataIndex: 'publishTime',
-    key: 'publishTime'
-  },
-  {
-    title: '标题',
-    dataIndex: 'title',
-    key: 'title',
-    ellipsis: true
-  },
-  {
-    title: '主要内容',
-    dataIndex: 'content',
-    key: 'content',
-    ellipsis: true
-  },
-  {
-    title: '操作',
-    dataIndex: 'operations',
-    key: 'operations',
-    scopedSlots: { customRender: 'operations' }
-  }
-]
-
-const data = [
-  {
-    key: '1',
-    ID: '1234',
-    publisher: '胡彦斌',
-    plane: '济人',
-    theme: '校园活动',
-    publishTime: '2017-10-01 12:00',
-    title: '关于新学期的一些和新学期有关的',
-    content: '同学们好，新学期来了，久学期过去了。'
-  }, {
-    key: '2',
-    ID: '1234',
-    publisher: '胡彦斌',
-    plane: '济人',
-    theme: '求职之路',
-    publishTime: '2017-10-01 12:00',
-    title: '关于新学期的一些和新学期有关的',
-    content: '同学们好，新学期来了，久学期过去了。'
-  }, {
-    key: '3',
-    ID: '1234',
-    publisher: '胡彦斌',
-    plane: '济事',
-    theme: '校园活动',
-    publishTime: '2017-10-01 12:00',
-    title: '关于新学期的一些和新学期有关的',
-    content: '同学们好，新学期来了，久学期过去了。'
-  }, {
-    key: '4',
-    ID: '1234',
-    publisher: '胡彦斌',
-    plane: '济事',
-    theme: '生活指南',
-    publishTime: '2017-10-01 12:00',
-    title: '关于新学期的一些和新学期有关的',
-    content: '同学们好，新学期来了，久学期过去了。'
-  }, {
-    key: '5',
-    ID: '1234',
-    publisher: '胡彦斌',
-    plane: '同德',
-    theme: '学习天地',
-    publishTime: '2017-10-01 12:00',
-    title: '关于新学期的一些和新学期有关的',
-    content: '同学们好，新学期来了，久学期过去了。'
-  }, {
-    key: '6',
-    ID: '1234',
-    publisher: '胡彦斌',
-    plane: '同德',
-    theme: '学习天地',
-    publishTime: '2017-10-01 12:00',
-    title: '关于新学期的一些和新学期有关的',
-    content: '同学们好，新学期来了，久学期过去了。'
-  }
-]
+import PublishedBox from '@/views/publish/PublishedBox'
+import DraftBox from '@/views/publish/DraftBox'
+import GarbageBox from '@/views/publish/GarbageBox'
 
 export default {
   name: 'PublishManagement',
+  components: { GarbageBox, DraftBox, PublishedBox },
   data () {
     return {
-      columns,
-      data
-    }
-  },
-  beforeCreate () {
-    this.form = this.$form.createForm(this, { name: 'search' })
-  },
-  methods: {
-    handleSubmit (e) {
-      e.preventDefault()
-      this.form.validateFields((err, value) => {
-        console.log(err)
-        console.log(value)
-      })
+      interface: 'published'
     }
   }
 }
