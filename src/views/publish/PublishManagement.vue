d79558c60aabe27e2eb5b79f28faf23d4713512b
Wei-Xiaoxing <281796586@qq.com>
2021-09-04 12:17:54 +0800

    制作帖子管理/新建帖子以及修复部分页面的警告
    1、完成帖子管理/新建帖子的制作。
    2、修复运营动态下两个页面和三个组件的大部分警告。

--- src/views/publish/PublishManagement.vue
+++ src/views/publish/PublishManagement.vue
@@ -2,15 +2,15 @@
   <page-header-wrapper>
     <a-card :bordered="false">
       <div class="table-page-search-wrapper">
-        <a-row :gutter='96'>
-          <a-col :span='1'><a-button :type='this.interface=="published"?"primary":"default"' @click='() => this.interface="published"'>已发布</a-button> </a-col>
-          <a-col :span='1'><a-button :type='this.interface=="draft"?"primary":"default"'@click='() => this.interface="draft"'>草稿箱</a-button></a-col>
-          <a-col :span='1'><a-button :type='this.interface=="garbage"?"primary":"default"'@click='() => this.interface="garbage"'>垃圾箱</a-button></a-col>
+        <a-row :gutter="96">
+          <a-col :span="1"><a-button :type="this.interface==='published'?'primary':'default'" @click="() => this.interface='published'">已发布</a-button> </a-col>
+          <a-col :span="1"><a-button :type="this.interface==='draft'?'primary':'default'" @click="() => this.interface='draft'">草稿箱</a-button></a-col>
+          <a-col :span="1"><a-button :type="this.interface==='garbage'?'primary':'default'" @click="() => this.interface='garbage'">垃圾箱</a-button></a-col>
         </a-row>
-        <div style='height: 20px'/>
-        <PublishedBox v-show='this.interface=="published"'></PublishedBox>
-        <DraftBox v-show='this.interface=="draft"'></DraftBox>
-        <GarbageBox v-show='this.interface=="garbage"'></GarbageBox>
+        <div style="height: 20px"/>
+        <PublishedBox v-show="this.interface==='published'"></PublishedBox>
+        <DraftBox v-show="this.interface==='draft'"></DraftBox>
+        <GarbageBox v-show="this.interface==='garbage'"></GarbageBox>
       </div>
     </a-card>
   </page-header-wrapper>
--- src/views/publish/PublishedBox.vue
+++ src/views/publish/PublishedBox.vue
@@ -1,69 +1,69 @@
 <template>
   <div>
-    <a-form layout='inline' :form='form' @submit='handleSubmit'>
-      <a-row :gutter='24'>
-        <a-col :span='2'>
-          <a-button type='primary'>刷新</a-button>
+    <a-form layout="inline" :form="form" @submit="handleSubmit">
+      <a-row :gutter="24">
+        <a-col :span="2">
+          <a-button type="primary">刷新</a-button>
         </a-col>
-        <a-col :span='3'>
-          <a-form-item label='动态ID'>
-            <a-input v-decorator='["id"]'/>
+        <a-col :span="3">
+          <a-form-item label="动态ID">
+            <a-input v-decorator="['id']"/>
           </a-form-item>
         </a-col>
-        <a-col :span='3'>
-          <a-form-item label='发布人'>
-            <a-select v-decorator='["publisher"]'>
+        <a-col :span="3">
+          <a-form-item label="发布人">
+            <a-select v-decorator="['publisher']">
             </a-select>
           </a-form-item>
         </a-col>
-        <a-col :span='3'>
-          <a-form-item label='板块'>
-            <a-select v-decorator='["plate"]'>
-              <a-select-option value='0'>济事</a-select-option>
-              <a-select-option value='1'>济人</a-select-option>
-              <a-select-option value='2'>同德</a-select-option>
+        <a-col :span="3">
+          <a-form-item label="板块">
+            <a-select v-decorator="['plate']">
+              <a-select-option value="0">济事</a-select-option>
+              <a-select-option value="1">济人</a-select-option>
+              <a-select-option value="2">同德</a-select-option>
             </a-select>
           </a-form-item>
         </a-col>
-        <a-col :span='3'>
-          <a-form-item label='主题'>
-            <a-select v-decorator='["theme"]'>
-              <a-select-option value='0'>求职信息</a-select-option>
-              <a-select-option value='1'>学习天地</a-select-option>
-              <a-select-option value='2'>校园活动</a-select-option>
-              <a-select-option value='3'>生活指南</a-select-option>
-              <a-select-option value='4'>竞赛</a-select-option>
-              <a-select-option value='5'>学术科研</a-select-option>
-              <a-select-option value='6'>一起造梦</a-select-option>
-              <a-select-option value='7'>其他</a-select-option>
+        <a-col :span="3">
+          <a-form-item label="主题">
+            <a-select v-decorator="['theme']">
+              <a-select-option value="0">求职信息</a-select-option>
+              <a-select-option value="1">学习天地</a-select-option>
+              <a-select-option value="2">校园活动</a-select-option>
+              <a-select-option value="3">生活指南</a-select-option>
+              <a-select-option value="4">竞赛</a-select-option>
+              <a-select-option value="5">学术科研</a-select-option>
+              <a-select-option value="6">一起造梦</a-select-option>
+              <a-select-option value="7">其他</a-select-option>
             </a-select>
           </a-form-item>
         </a-col>
-        <a-col :span='3'>
+        <a-col :span="3">
           <a-form-item>
-            <a-date-picker v-decorator='["beginTime"]'/>
+            <a-date-picker v-decorator="['beginTime']"/>
           </a-form-item>
         </a-col>
-        <a-col :span='3'>
+        <a-col :span="3">
           <a-form-item>
-            <a-date-picker v-decorator='["endTime"]'/>
+            <a-date-picker v-decorator="['endTime']"/>
           </a-form-item>
         </a-col>
-        <a-col :span='1'>
-          <a-button type='primary' html-type='submit'>筛选</a-button>
+        <a-col :span="1">
+          <a-button type="primary" html-type="submit">筛选</a-button>
         </a-col>
-        <a-col :span='1' :offset='1'>
-          <a-button type='primary'>重置</a-button>
+        <a-col :span="1" :offset="1">
+          <a-button type="primary">重置</a-button>
         </a-col>
       </a-row>
     </a-form>
 
-    <a-table :columns='columns' :data-source='data' >
-        <span slot='operations'>
-          <a>查看原帖 </a>
-          <a>修改 </a>
-          <a>删除</a>
-        </span>
+    <a-table :columns="columns" :data-source="data" >
+      <span slot="operations">
+        <a>查看原帖 </a>
+        <a>修改 </a>
+        <a>删除</a>
+      </span>
     </a-table >
   </div>
 </template>
