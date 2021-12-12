411a58b9645da2991d4fdeddada2b5032711cb26
前端爱码士 <a63149300@163.com>
2021-12-12 23:27:32 +0800

    解决登录页logo.svg不显示BUG
    解决img标签src属性显示[object Module]BUG

--- vue.config.js
+++ vue.config.js
@@ -70,7 +70,8 @@ const vueConfig = {
       .use('file-loader')
       .loader('file-loader')
       .options({
-        name: 'assets/[name].[hash:8].[ext]'
+        name: 'assets/[name].[hash:8].[ext]',
+        esModule: false
       })
 
     // if prod is on
