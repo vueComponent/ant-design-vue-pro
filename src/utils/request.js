0f1fec55c885ff828905405092e7a69d936ae05c
celeenobug <46836357+celeenobug@users.noreply.github.com>
2022-01-23 06:29:53 +0800

    Update request.js
    token协议头名称没有同store关联起来，如果在@/store/mutation-types中修改了名称，将会失效。

--- src/utils/request.js
+++ src/utils/request.js
@@ -47,7 +47,7 @@ request.interceptors.request.use(config => {
   // 如果 token 存在
   // 让每个请求携带自定义 token 请根据实际情况自行修改
   if (token) {
-    config.headers['Access-Token'] = token
+    config.headers[ACCESS_TOKEN] = token
   }
   return config
 }, errorHandler)
